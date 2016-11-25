160898cef57eb6d02d89889eba60626c69148bfd
Jonayet <jonayet.bu@gmail.com>
2016-11-25 23:52:33 +0600

    router strategy set to hash-strategy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 import { BrowserModule }  from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { MaterialModule } from '@angular/material';
@@ -16,6 +17,7 @@ import {routes} from './app.router';
         NewsFeedComponent,
         AppComponent
     ],
+    providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
     bootstrap: [ AppComponent ]
 })
 export class AppModule { }
